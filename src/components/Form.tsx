0411ab360e80e890f9650dd4e623d73c2b1d3275
ddimkou <ddimkou@outlook.com>
2023-08-04 15:45:11 +0300

    handling errors

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,17 @@
 import { useRef } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  name: string;
+  age: number;
+}
+
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
@@ -14,11 +23,19 @@ const Form = () => {
             Name:
           </label>
           <input
-            {...register("name")}
+            {...register("name", { required: true, minLength: 3 })}
             id="name"
             type="text"
             className="form-control"
           />
+          {errors.name?.type === "required" && (
+            <p className="text-danger">The name field is required.</p>
+          )}
+          {errors.name?.type === "minLength" && (
+            <p className="text-danger">
+              The name must be at least 3 characters.
+            </p>
+          )}
         </div>
         <div className="mb-3">
           <label htmlFor="age" className="form-label">
